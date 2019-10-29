dead36c0d85003110ae534b9c336c022beb7d415
Andrew Richards <arichards4814@gmail.com>
2019-10-29 12:42:56 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,28 @@
 # Your Code Here
+def map(arr)
+  new = []
+  i = 0 
+  while i < arr.length do
+  
+    new.push(yield(arr[i]))
+    i += 1  
+  end
+  return new
+end
+
+def reduce(arr, start = nil)
+  i = 0
+  
+  if start
+  total = start  
+  else
+    
+  end
+  
+  while i < arr.length
+  total = yield(start, arr[i])
+  
+  i += 1 
+  end
+  
+end
